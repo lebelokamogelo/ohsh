43e6df88e3c8f881fc31ff33186b900a78ae52dc
lebelokamogelo <lebelokamogelo47@gmail.com>
2025-05-16 09:28:32 +0200

    read_line

--- src/main.c
+++ src/main.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define OHSH_BUFSIZE 1024
+
 void ohsh_loop(void);
+char *ohsh_read_line(void);
 
 int main(int argc, char const *argv[])
 {
@@ -20,6 +23,10 @@ void ohsh_loop(void)
     do
     {
         printf("> ");
+
+                // Read: Read the command from standard input.
+        // Parse: Separate the command string into a program and arguments.
+        // Execute: Run the parsed command.
         line = ohsh_read_line();
         args = ohh_split_line(line);
         status = ohsh_execute(args);
@@ -27,4 +34,48 @@ void ohsh_loop(void)
         free(line);
         free(args);
     } while (status);
+}
+
+char *ohsh_read_line(void)
+{
+    int bufsize = OHSH_BUFSIZE;
+    int position = 0;
+    char *buffer = malloc(sizeof(char) * bufsize);
+    int c;
+
+    if (!buffer)
+    {
+        fprintf(stderr, "ohsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1)
+    {
+        // Read a character
+        c = getchar();
+
+        // If EOF, replace it with a null character and return.
+        if (c == EOF || c == '\n')
+        {
+            buffer[position] = '\0';
+            return buffer;
+        }
+        else
+        {
+            buffer[position] = c;
+        }
+        position++;
+
+        // If buffer exceeded then reallocate.
+        if (position >= bufsize)
+        {
+            bufsize += OHSH_BUFSIZE;
+            buffer = realloc(buffer, bufsize);
+            if (!buffer)
+            {
+                fprintf(stderr, "ohsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
 }
